1c941fe27797b236d08e186f7c266cfcb2744d56
Kirill Fedyakov <kfedyakov@easirun.de>
2020-12-23 13:27:41 +0100

    model Pool added

--- api/api_v1/models.py
+++ api/api_v1/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Pool(models.Model):
+    title = models.CharField('Опрос', max_length=100)
+    description = models.TextField('Описание', max_length=500)
+    start_date = models.DateTimeField('Дата старта', auto_now_add=True)
+    end_date = models.DateTimeField('Дата окончания')
