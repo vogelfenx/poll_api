baf29251eaf5004517030635f5e09d2166f56f6f
Kirill Fedyakov <vogelfenix@gmail.com>
2020-12-25 16:02:52 +0200

    small changes

--- devenv_setup.bat
+++ devenv_setup.bat
@@ -1,3 +1,4 @@
+@echo off
 rem step 1: create virtual env
 python -m venv .venv
 
@@ -8,12 +9,8 @@ rem step 3: install dependencies
 pip install -r .venv\requirements.txt
 
 rem step 4: setup database
-python api\manage.py makemigrations
-python api\manage.py migrate
+rem python api\manage.py makemigrations
+rem python api\manage.py migrate
 
-rem step 5: create super user
-python api\manage.py createsuperuser --email admin@example.com --username admin
-
-rem step 6: load test data into db
-
+@echo on
 cmd /k
